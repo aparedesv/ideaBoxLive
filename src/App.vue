b6e20d424963aed97dcd9cb12ef1be8b60cc02da
Andreu Paredes <aparedesverdaguer@gmail.com>
2021-01-01 16:03:43 +0100

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- main container -->
+  <div class="container mx-auto p-4">
+    <!-- main box -->
+    <div class="w-full bg-gray-100 shadow-lg p-4 rounded-lg">
+      <h1 class="mb-5 text-4xl text-center">
+        IdeaBox
+      </h1>
+
+      <!-- add idea -->
+      <section class="mb-6">
+        <form class="sm:flex">
+          <input
+            class="w-full p-3 sm:flex-auto"
+            type="text"
+            required
+            placeholder="add your idea"
+          />
+          <input
+            class="w-full p-2 bg-gray-600 text-white sm:flex-1"
+            type="submit"
+            value="Add Idea"
+          />
+        </form>
+        <p v-if="user">Please <a @click="doLogin" href="#">Login</a> first</p>
+        <p v-else>
+          Hi {{ user.displayName }}.<a @click="doLogout" href="#">Logout</a>
+        </p>
+      </section>
+      <!-- end add idea -->
+
+      <AppIdea v-for="(idea, index) in ideas" :key="index" :idea="idea" />
+    </div>
+    <!-- end main box -->
+  </div>
+  <!-- end main container -->
+</template>
+
+<script>
+import AppIdea from "@/components/AppIdea.vue";
+import seed from "@/seed.json";
+import { ref } from "vue";
+import { auth } from "@/firebase.js";
+
+export default {
+  name: "App",
+  setup() {
+    const ideas = ref(seed.ideas);
+    let user = ref(null);
+    // auth.onAuthStateChanged(async auth => (user.value = auth ? auth : null));
+    console.log(auth);
+    return {
+      ideas,
+      user
+    };
+  },
+  components: {
+    AppIdea
+  }
+};
+</script>
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2EeuViLwH5f2_0op-w6PryIeDO0Bsxt4",
+  authDomain: "ideaboxlive-e608a.firebaseapp.com",
+  projectId: "ideaboxlive-e608a",
+  storageBucket: "ideaboxlive-e608a.appspot.com",
+  messagingSenderId: "1022616628898",
+  appId: "1:1022616628898:web:debcae8276ef106673b1fb"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+// const db = firebase.firestore();
+
+export { auth };
